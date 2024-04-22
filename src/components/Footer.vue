3ea7bce7546cd7229594257095418747e92794a4
victorsantanna <victor.esef@hotmail.com>
2024-04-22 13:25:24 -0300

    feat: criação do footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>footer</p>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name:"Footer",
+    
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Treino.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Treino</h1>
+
+  </div>
+</template>
